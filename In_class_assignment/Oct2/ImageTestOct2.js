e93519233e03c9e4806fcf90078c180a98e4fa09
Kfish247 <55206139+Kfish247@users.noreply.github.com>
2019-10-02 14:39:58 -0500

    update

---
+++ In_class_assignment/Oct2/ImageTestOct2.js
@@ -0,0 +1,19 @@
+
+function setup(){
+
+    createCanvas(windowWidth, windowHeight);
+    
+ 
+}
+ 
+function draw(){
+ 
+}
+
+
+
+function windowResized(){
+
+    resizeCanvas(windowWidth,windowHeight);
+ 
+}
