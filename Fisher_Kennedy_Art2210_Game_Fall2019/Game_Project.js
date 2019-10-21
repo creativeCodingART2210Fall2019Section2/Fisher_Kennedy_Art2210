f044ee4993837dc586ac47f43e04dae97bebba7e
Kfish247 <55206139+Kfish247@users.noreply.github.com>
2019-10-21 09:58:19 -0500

    Update Game_Project.js

--- Fisher_Kennedy_Art2210_Game_Fall2019/Game_Project.js
+++ Fisher_Kennedy_Art2210_Game_Fall2019/Game_Project.js
@@ -1,13 +1,17 @@
-//var Hue = 0;
-//var col = {
- // r: 255,
- // g: 0,
- // b: 0
-//};
+var Hue = 0;
+var col = {
+r: 255,
+g: 0,
+b: 0
+};
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(255,175,175);
+	colorMode(HSB, 90);
+	background(Hue, 90, 90);
+	Hue = (Hue + 0.05) % 100;
+	//HSB colormode
+	//background(255,175,175); PINK BACKGROUND
 	{
 	//Tic-Tac-Toe lines 
     strokeWeight(20)	
@@ -16,6 +20,11 @@ function setup() {
 	line(200,250,1100,250)
 	line(200,500,1100,500);
 	}
+}
+
+function draw(){
+	fill(value)
+	rectangle(700,700,250,500);
 }
 
 function keyTyped() {
@@ -37,13 +46,6 @@ function keyTyped() {
 		ellipse(mouseX,mouseY,100,100);
 	}
 }
-
-//function draw(){
-	//colorMode(HSB, 90);
-	//background(Hue, 90, 90);
-	//Hue = (Hue + 0.05) % 100;
-	//HSB colormode 
-//}
 
 function windowResized(){
 
